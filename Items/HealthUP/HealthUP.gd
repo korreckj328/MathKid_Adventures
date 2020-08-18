c8dcbe9b9ae6ae4cb6eb6c0c39cf5f45ce342fcf
Jeremiah Korreck <korreckj328@gmail.com>
2020-08-18 16:51:10 -0400

    Added tween and powerup sound

--- Items/HealthUP/HealthUP.gd
+++ Items/HealthUP/HealthUP.gd
@@ -2,9 +2,20 @@ extends Area2D
 
 signal healthPickup
 
+var onBoard
+onready var tween = $Tween
+
 func _ready():
-	pass
+	onBoard = true
+	tween.interpolate_property($Sprite, 'scale', $Sprite.scale, $Sprite.scale * 3, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.interpolate_property($Sprite, 'modulate', Color(1,1,1,1), Color(1,1,1,0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 
 func _on_HealthUP_body_entered(_body):
-	emit_signal("healthPickup")
+	if onBoard:
+		$Pickup.play()
+		emit_signal("healthPickup")
+		tween.start()
+
+
+func _on_Tween_tween_completed(_object, _key):
 	queue_free()
