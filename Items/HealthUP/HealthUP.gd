9c443314e5e75ac5971fa60b91b330981170311a
Jeremiah Korreck <korreckj328@gmail.com>
2020-08-20 20:28:19 -0400

    Added a fade in effect on the Health Pickup

--- Items/HealthUP/HealthUP.gd
+++ Items/HealthUP/HealthUP.gd
@@ -3,18 +3,22 @@ extends Area2D
 signal healthPickup
 
 var onBoard
-onready var tween = $Tween
+onready var pickUpTween = $PickupTween
+onready var fadeIn = $Fadein
 
 func _ready():
 	onBoard = true
-	tween.interpolate_property($Sprite, 'scale', $Sprite.scale, $Sprite.scale * 3, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-	tween.interpolate_property($Sprite, 'modulate', Color(1,1,1,1), Color(1,1,1,0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	pickUpTween.interpolate_property($Sprite, 'scale', $Sprite.scale, $Sprite.scale * 3, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	pickUpTween.interpolate_property($Sprite, 'modulate', Color(1,1,1,1), Color(1,1,1,0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	fadeIn.interpolate_property($Sprite, 'scale', $Sprite.scale / 4, $Sprite.scale, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	fadeIn.interpolate_property($Sprite, 'modulate', Color(1,1,1,0), Color(1,1,1,1), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	fadeIn.start()
 
 func _on_HealthUP_body_entered(_body):
 	if onBoard:
 		$Pickup.play()
 		emit_signal("healthPickup")
-		tween.start()
+		pickUpTween.start()
 
 
 func _on_Tween_tween_completed(_object, _key):
